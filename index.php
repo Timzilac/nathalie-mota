3d611ac72de562826fe7de553b1a6a05beba73e7
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-25 21:56:00 -0600

    banner

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
-<h1>Bienvenue sur le site CookInFamily !</h1>
+<section class="banner">
+    <img class="image-banner" src="<?php echo get_template_stylesheet_uri(); ?>images/banner.jpg" alt="PHOTOGRAPHE EVENT" height="962" width="1440"></img>
+    <h1 class="titre-banner">PHOTOGRAPHE EVENT</h1>
+</section>
 <?php get_footer(); ?>
