9f706c7a4b47475c46d06c43e845374b2aac329b
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-25 22:24:33 -0600

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,16 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>style.css" />
         <title>Nathalie Mota</title>
     </head>
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
