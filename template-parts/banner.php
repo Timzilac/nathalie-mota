4d3e9e48180d0d266cdaaf669f3a0a28b8545fae
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-25 22:07:07 -0600

    fix

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,4 +1,4 @@
 <section class="banner">
-    <img class="image-banner" src="<?php echo get_template_stylesheet_uri(); ?>/images/banner.jpg" alt="PHOTOGRAPHE EVENT" height="962" width="1440"></img>
+    <img class="image-banner" src="<?php echo get_template_directory_uri(); ?>/images/banner.jpg" alt="PHOTOGRAPHE EVENT" height="962" width="1440"></img>
     <h1 class="titre-banner">PHOTOGRAPHE EVENT</h1>
 </section>
