c984174f5ba35d37062746edfdbea6cf513498ba
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-25 22:05:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <h1>Bienvenue sur le site CookInFamily !</h1>
 
+<?php include get_theme_file_path('template-parts/banner.php'); ?>
+
 <?php get_template_part('template-parts/banner'); ?>
 
 <?php get_footer(); ?>
