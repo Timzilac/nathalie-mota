0573ca66bf1c83eda9d503b7432faa86dbf08388
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-28 18:10:15 -0600

    Update functions.php

---
+++ functions.php
@@ -0,0 +1,4 @@
+function enqueue_modal_script() {
+    wp_enqueue_script('modal-script', get_template_directory_uri() . '/js/modal.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_modal_script');
