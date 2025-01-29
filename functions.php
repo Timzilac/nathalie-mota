9767c674bd85063f26b0f6f7d3043a8b2c15a69a
Timothe Lacoste <timothe.lacostewp@gmail.com>
2025-01-28 18:17:01 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
-function enqueue_modal_script() {
-    wp_enqueue_script('modal-script', get_template_directory_uri() . '/js/modal.js', array(), null, true);
+function theme_enqueue_scripts() {
+// Ajouter le fichier modal.js
+wp_enqueue_script(
+'modal-script',
+get_template_directory_uri() . 'js\modal.js',
+true // Charger dans le footer
+);
 }
-add_action('wp_enqueue_scripts', 'enqueue_modal_script');
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
